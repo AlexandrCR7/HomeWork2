eb7c9e22b80e5cda8505ea14effe842debf3e16d
Alexandr <k9108@mail.ru>
2023-04-13 15:58:50 +0300

    Imagine silving the problem 15v3

--- HomeWork15v3/Program.cs
+++ HomeWork15v3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿System.Console.WriteLine("Введите день недели: ");
+int dayOfWeek = int.Parse(System.Console.ReadLine());
+int dayOfWeek1 = dayOfWeek - 1;
+
+if(dayOfWeek1 <= 5)
+{
+    System.Console.WriteLine("Это рабочий день");
+    string[] array = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+    System.Console.WriteLine(array[dayOfWeek1]);
+}
+else
+{
+System.Console.WriteLine("Это выходной");
+string[] array = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+System.Console.WriteLine(array[dayOfWeek1]);
+}
