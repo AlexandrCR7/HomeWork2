0309481c64d5e41dae87f59a7120fd4380e89db7
Alexandr <k9108@mail.ru>
2023-04-13 16:10:17 +0300

    Imagine silving the problem 15v3

--- HomeWork15/Program.cs
+++ HomeWork15/Program.cs
@@ -1,32 +1,25 @@
-﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-
-
-// System.Console.WriteLine("Введите день недели: ");
-// int command = int.Parse(Console.ReadLine());
-// switch (command){
-//     case 1:
-//         System.Console.WriteLine("Работать!");
-//         break;
-//     case 2:
-//         System.Console.WriteLine("Работать!");
-//         break;
-//     case 3:
-//         System.Console.WriteLine("Работать!");
-//         break;
-//     case 4:
-//         System.Console.WriteLine("Работать!");
-//         break;
-//     case 5:
-//         System.Console.WriteLine("Работать!");
-//         break;
-//     case 6:
-//         System.Console.WriteLine("Отдыхаем");
-//         break;
-//     case 7:
-//         System.Console.WriteLine("Расслабляемся");
-//         break;
-// }
+﻿System.Console.WriteLine("Введите день недели: ");
+int command = int.Parse(Console.ReadLine());
+switch (command){
+    case 1:
+        System.Console.WriteLine("Работать!");
+        break;
+    case 2:
+        System.Console.WriteLine("Работать!");
+        break;
+    case 3:
+        System.Console.WriteLine("Работать!");
+        break;
+    case 4:
+        System.Console.WriteLine("Работать!");
+        break;
+    case 5:
+        System.Console.WriteLine("Работать!");
+        break;
+    case 6:
+        System.Console.WriteLine("Отдыхаем");
+        break;
+    case 7:
+        System.Console.WriteLine("Расслабляемся");
+        break;
+}
